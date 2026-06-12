3c9b48f151b30ccd0d40542e7bb34ec3251ee02c
LasithaOffice <awesometutes@gmail.com>
2026-06-12 18:51:49 +0530

    changed the base url ip

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const apiClient = axios.create({
-  baseURL: "http://172.20.10.5:3001/api/",
+  baseURL: "http://10.0.2.2:3001/api/",
   //"http://172.20.10.5:3001/api/" + "properties"
   headers: {
     'Content-Type': 'application/json',
